dc7de34084214b912f0c4ff81cae881201c42f65
0730chen <1252331215@qq.com>
2020-07-27 23:00:29 +0800

    feat:修改id添加方法

---
+++ lib/createId.ts
@@ -0,0 +1,7 @@
+let index = 0
+const createId =  ()=>{
+    index +=1
+    return index
+}
+
+export {createId}
--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,7 +1,7 @@
 import {Body, Controller, Get, HttpStatus, Post, Res} from '@nestjs/common';
 import {UserService} from "./user.service";
 import {CreateUserDTO} from "./dto/user.dto";
-let index = 0
+import {createId} from "../../lib/createId";
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) { }
@@ -15,15 +15,13 @@ export class UserController {
     @Post('createUser')
     async createAccount(@Body() createAccountDto:CreateUserDTO,@Res() res) {
         let User = {
-            _id:index+1,
+            _id:createId(),
             name:createAccountDto.name,
             password:createAccountDto.password,
             tags:[],
             records:[]
         }
         const posts = await this.userService.createUser(User);
-        console.log(index);
-        index +=1
         return res.status(HttpStatus.OK).json(posts);
     }
 
